6c0af01dbc65d4b9470f6ecf8d066bcc8bd70451
PatrikP <patrik.pukanec@gmail.cz>
2020-07-21 21:53:11 +0200

    new category form

--- src/components/Todos/Sidebar.js
+++ src/components/Todos/Sidebar.js
@@ -1,24 +1,36 @@
 import React, { useState } from 'react'
 import SidebarItem from './SidebarItem'
-import addIcon from '../../resources/icons/add.svg'
+import plusIcon from '../../resources/icons/plus.svg'
+import minusIcon from '../../resources/icons/minus.svg'
 
 export default function Sidebar() {
     const [isActive, setActive] = useState(false)
+
+    const onSubmit = () => console.log('hi new category')
 
     return (
         <div className='sidebar'>
             <div className='categories-top'>
                 <h3 className='categories-heading'>Categories</h3>
-                <button className='new-category-btn'>
-                    <img className='category-btn-icon' alt='' src={addIcon}/>
+                {
+                isActive ?
+                <button className='new-category-btn' onClick={() => setActive(false)}>
+                    <img className='category-btn-icon' alt='' src={minusIcon} />
                 </button>
+                :
+                <button className='new-category-btn' onClick={() => setActive(true)}>
+                    <img className='category-btn-icon' alt='' src={plusIcon}/>
+                </button>
+                }
             </div>
             <form className={`category-form ${isActive && 'active'}`}>
                 <input
                     className='category-input'
                     type='text'
+                    placeholder='Add new category'
                     required 
                 />
+                <button className='category-submit-btn' type='submit' onClick={onSubmit}>Add</button>
             </form>
             <ul className='categories'>
                 <SidebarItem linkTo={'/todos/work'} value={'Work'} isActive={true} />
