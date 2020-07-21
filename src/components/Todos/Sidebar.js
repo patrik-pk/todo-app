14759ace9d09188c5d5ab88b3f68d9d7afadb6a5
PatrikP <patrik.pukanec@gmail.cz>
2020-07-21 21:31:34 +0200

    categories html css

--- src/components/Todos/Sidebar.js
+++ src/components/Todos/Sidebar.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SidebarItem from './SidebarItem'
+import addIcon from '../../resources/icons/add.svg'
 
 export default function Sidebar() {
+    const [isActive, setActive] = useState(false)
+
     return (
         <div className='sidebar'>
-            <h3 className='categories-heading'>Categories</h3>
+            <div className='categories-top'>
+                <h3 className='categories-heading'>Categories</h3>
+                <button className='new-category-btn'>
+                    <img className='category-btn-icon' alt='' src={addIcon}/>
+                </button>
+            </div>
+            <form className={`category-form ${isActive && 'active'}`}>
+                <input
+                    className='category-input'
+                    type='text'
+                    required 
+                />
+            </form>
             <ul className='categories'>
                 <SidebarItem linkTo={'/todos/work'} value={'Work'} isActive={true} />
                 <SidebarItem linkTo={'/todos/exercise'} value={'Exercise'} isActive={false} />
