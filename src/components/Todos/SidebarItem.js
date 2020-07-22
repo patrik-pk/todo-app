cf58474cf9bfe3902d511ef22f7a93692622dce6
PatrikP <patrik.pukanec@gmail.cz>
2020-07-22 23:49:58 +0200

    category item styling

--- src/components/Todos/SidebarItem.js
+++ src/components/Todos/SidebarItem.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { setActiveCategory } from '../../actions/todoActions'
+import deleteIcon from '../../resources/icons/delete.svg'
 
 function SidebarItem({ category, setActiveCategory }) {
     const { linkTo, value, isActive } = category
@@ -10,14 +11,17 @@ function SidebarItem({ category, setActiveCategory }) {
     const onClick = () => setActiveCategory(category)
     
     return (
-        <li className='category-item'>
+        <li className={`category-item ${isActive ? 'active' : ''}`}>
             <Link 
-                className={`category-link ${isActive && 'active'}`} 
+                className='category-link' 
                 to={linkTo}
                 onClick={onClick}
             >
             {value.charAt(0).toUpperCase() + value.slice(1)}
             </Link>
+            <button className='category-delete-btn'>
+                <img className='delete-icon' alt='' src={deleteIcon} />
+            </button>
         </li>
     )
 }
