8d1cc65540bd27e3ac3694c04e8804b6df12168a
PatrikP <patrik.pukanec@gmail.cz>
2020-07-24 00:49:43 +0200

    category form validation
    create new category only, if the same doesnt exist + added redirect to the newly created category

--- src/components/Todos/SidebarForm.js
+++ src/components/Todos/SidebarForm.js
@@ -1,26 +1,48 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { addCategory, showCategoryForm } from '../../actions/todoActions'
+import { addCategory, showCategoryForm, setActiveCategory } from '../../actions/todoActions'
 import { v4 as uuidv4 } from 'uuid'
 import plusIcon from '../../resources/icons/plus.svg'
 
-function SidebarForm({ formActive, showCategoryForm, addCategory }) {
-    // newCategory is used for the input value
-    const [newCategory, setNewCategory] = useState('')
+function SidebarForm({ formActive, categories, showCategoryForm, addCategory, setActiveCategory }) {
+    const history = useHistory()
+    const [categoryInput, setCategoryInput] = useState('')
 
-    const onChange = (e) => setNewCategory(e.target.value)
+    const onChange = (e) => setCategoryInput(e.target.value)
+
+    // Filter the categories, if there is a category with
+    // same value case insensitive, return true
+    const categoryExists = (categories) => {
+        const filtered = categories.filter(category => {
+            return category.value.toLowerCase() === categoryInput.toLowerCase()
+        })
+        return filtered.length > 0 ? true : false 
+    }
 
     const onSubmit = (e) => {
         e.preventDefault()
-        addCategory({
-            id: uuidv4(),
-            linkTo: `/todos/${newCategory}`,
-            value: newCategory,
-            isActive: false
-        })
-        setNewCategory('')
-        showCategoryForm(false)
+        // If category already exists
+        if(categoryExists(categories)) {
+            // Add warning box in the future
+            console.log(`Category ${categoryInput} already exists.`)
+        } // If it doesn't, create one 
+        else {
+            const id = uuidv4()
+            const linkTo = `/todos/${categoryInput}`
+            // Create new category
+            addCategory({
+                id,
+                linkTo,
+                value: categoryInput,
+                isActive: false
+            })       
+            setCategoryInput('') // clear input
+            showCategoryForm(false) // hide form
+            setActiveCategory({ id }) // set created category to active
+            history.push(linkTo) // redirect to created category
+        }
     }
 
     return (
@@ -30,7 +52,7 @@ function SidebarForm({ formActive, showCategoryForm, addCategory }) {
                 type='text'
                 placeholder='Add new category'
                 onChange={onChange}
-                value={newCategory}
+                value={categoryInput}
                 required
             />
             <button className='category-submit-btn' type='submit'>
@@ -41,12 +63,16 @@ function SidebarForm({ formActive, showCategoryForm, addCategory }) {
 }
 
 const mapStateToProps = state => ({
-    formActive: state.todo.categoryFormShown
+    formActive: state.todo.categoryFormShown,
+    categories: state.todo.categories
 })
 
 SidebarForm.propTypes = {
     addCategory: PropTypes.func.isRequired,
+    setActiveCategory: PropTypes.func.isRequired,
+    showCategoryForm: PropTypes.func.isRequired,
     formActive: PropTypes.bool.isRequired,
+    categories: PropTypes.array.isRequired,
 }
 
-export default connect(mapStateToProps, { showCategoryForm, addCategory })(SidebarForm)
+export default connect(mapStateToProps, { showCategoryForm, addCategory, setActiveCategory })(SidebarForm)
