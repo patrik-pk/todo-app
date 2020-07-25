8fea240e3614c7b1c2e52d406f8a280617c985de
PatrikP <patrik.pukanec@gmail.cz>
2020-07-25 22:21:18 +0200

    add active category to todo when adding / updating todo

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -5,7 +5,7 @@ import { addTodo, updateTodo, clearCurrentTodo } from '../../actions/todoActions
 import { v4 as uuidv4 } from 'uuid'
 import clearIcon from '../../resources/icons/clear.svg'
 
-function TodoForm({ addTodo, updateTodo, current, clearCurrentTodo }) {
+function TodoForm({ addTodo, updateTodo, current, clearCurrentTodo, categories }) {
     // The "current" variable is filled with the given todo
     // when the edit button is clicked
     // So when there is something add "active" class
@@ -14,10 +14,17 @@ function TodoForm({ addTodo, updateTodo, current, clearCurrentTodo }) {
 
     const [task, setTask] = useState('')
     
+    // Watch current
     useEffect(() => {
         // Set input value to current, if there is one 
         if(current !== null) setTask(current.task)
     }, [current])
+
+    // Get Active Category
+    const getActiveCategory = categories => {
+        const activeCat = categories.filter(category => category.isActive)
+        return activeCat[0].value
+    }
 
     // Clear current
     const onClear = () => {
@@ -36,6 +43,7 @@ function TodoForm({ addTodo, updateTodo, current, clearCurrentTodo }) {
             addTodo({ 
                 id: uuidv4(), 
                 task,
+                category: getActiveCategory(categories),
                 isCompleted: false 
             })
             setTask('')
@@ -44,6 +52,7 @@ function TodoForm({ addTodo, updateTodo, current, clearCurrentTodo }) {
             updateTodo({ 
                 id: current.id, 
                 task, 
+                category: getActiveCategory(categories),
                 isCompleted: current.isCompleted
             })
             clearCurrentTodo()
@@ -74,11 +83,13 @@ function TodoForm({ addTodo, updateTodo, current, clearCurrentTodo }) {
 
 const mapStateToProps = state => ({
     todos: state.todo.todos,
-    current: state.todo.current
+    current: state.todo.current,
+    categories: state.todo.categories
 })
 
 TodoForm.propTypes = {
     todos: PropTypes.array.isRequired,
+    categories: PropTypes.array.isRequired,
     addTodo: PropTypes.func.isRequired,
     updateTodo: PropTypes.func.isRequired,
     clearCurrentTodo: PropTypes.func.isRequired
